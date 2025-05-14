fdcae9201352bca819897c7f653075b8f1758152
IT-Sensync <it.sensync@gmail.com>
2025-05-14 10:35:26 +0700

    Revert "build config"
    This reverts commit 2d6b52b17fb51e019d3a674171977be48c6daa73.

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // base: "/dashboard/",
-  preview: {
-    host: true,
-    port: 5004,
-    allowedHosts: ['absensi.getsensync.com']
-  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
