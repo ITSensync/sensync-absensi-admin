2d6b52b17fb51e019d3a674171977be48c6daa73
root <root@debian-s-1vcpu-2gb-amd-sgp1-01>
2025-05-14 10:22:11 +0700

    build config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // base: "/dashboard/",
+  preview: {
+    host: true,
+    port: 5004,
+    allowedHosts: ['absensi.getsensync.com']
+  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
