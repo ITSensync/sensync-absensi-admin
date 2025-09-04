b8c2d7cd297572fb94162f3f1bc6bbfafbcdae46
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-09-04 16:42:20 +0700

    feat: slicing card dashboard

---
+++ src/components/CardName.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { mdiCheckBold, mdiCloseThick } from '@mdi/js'
+import BaseIcon from './BaseIcon.vue'
+import CardBox from './CardBox.vue'
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'Alief',
+  },
+  checkIn: {
+    type: String,
+    default: '07:00',
+  },
+  lastUpdate: {
+    type: String,
+    default: '07:00',
+  },
+})
+
+const last_update = ref(props.lastUpdate)
+const timeNow = ref(new Date())
+const notConnect = ref(false)
+
+watch(
+  timeNow,
+  (newTime) => {
+    const today = new Date().toISOString().split('T')[0] // "2025-09-04"
+    const lastUpdateDate = new Date(`${today}T${last_update.value}`) // "2025-09-04T08:52:50"
+
+    const diffMin = (newTime - lastUpdateDate) / 60000
+    if (diffMin > 10) {
+      notConnect.value = true
+    }
+  },
+  { immediate: true },
+)
+</script>
+
+<template>
+  <CardBox
+    rounded="rounded-lg"
+    class="shadow-[0px_0px_5px_rgba(0,0,0,0.3)]"
+    :class="
+      notConnect
+        ? 'shadow-red-200 animate-[pulseShadowNotConnect_2s_ease-in-out_infinite]'
+        : 'shadow-emerald-200 animate-[pulseShadowConnect_2s_ease-in-out_infinite]'
+    "
+  >
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-col">
+        <p class="text-3xl font-semibold">{{ props.name }}</p>
+        <p class="text-zinc-400 text-xs">
+          Check-In: <span class="font-bold italic">{{ props.checkIn }}</span>
+        </p>
+        <p class="text-zinc-400 text-xs">
+          Update: <span class="font-bold italic">{{ props.lastUpdate }}</span>
+        </p>
+      </div>
+      <BaseIcon
+        :path="notConnect ? mdiCloseThick : mdiCheckBold"
+        size="48"
+        w=""
+        h="h-16"
+        :class="notConnect ? 'text-red-200' : 'text-emerald-200'"
+      />
+    </div>
+  </CardBox>
+</template>
+
+<style>
+@keyframes pulseShadowConnect {
+  0%,
+  100% {
+    box-shadow: 0 0 10px rgba(164, 244, 207, 0.2); /* redup */
+  }
+  50% {
+    box-shadow: 0 0 11px rgba(164, 244, 207, 0.8); /* nyala */
+  }
+}
+
+@keyframes pulseShadowNotConnect {
+  0%,
+  100% {
+    box-shadow: 0 0 10px rgba(244, 164, 164, 0.2); /* redup */
+  }
+  50% {
+    box-shadow: 0 0 11px rgba(244, 164, 164, 0.8); /* nyala */
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable no-unused-vars -->
 <script setup>
-import { ref, watch } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 import { useMainStore } from '@/stores/main'
 import { mdiAccountMultiple, mdiChartTimelineVariant, mdiCalendar, mdiCalendarMonth } from '@mdi/js'
 import * as chartConfig from '@/components/Charts/chart.config.js'
@@ -11,6 +11,8 @@ import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
 import TableListPresence from '@/components/TableListPresence.vue'
 import CardBoxComponentEmpty from '@/components/CardBoxComponentEmpty.vue'
+import CardName from '@/components/CardName.vue'
+import SectionTitle from '@/components/SectionTitle.vue'
 
 const chartData = ref(null)
 
@@ -26,11 +28,14 @@ const dataPresenceToday = ref([])
 // onMounted(() => {
 //   fillChartData()
 // })
-
+let intervalId = null
 const dateNow = new Date()
-const currentMonth = dateNow.toISOString().slice(0, 7);
-mainStore.fetchPresenceToday()
-mainStore.fetchPresenceMonthly(currentMonth)
+const currentMonth = dateNow.toISOString().slice(0, 7)
+const fetchData = () => {
+  mainStore.fetchPresenceToday()
+  mainStore.fetchPresenceMonthly(currentMonth)
+}
+
 watch(
   [() => mainStore.presenceToday, () => mainStore.presenceMonthly],
   ([newToday, newMonthly]) => {
@@ -40,6 +45,19 @@ watch(
   },
 )
 
+onMounted(() => {
+  fetchData() // fetch pertama kali saat page render
+
+  // ulangi setiap 5 menit (300000 ms)
+  intervalId = setInterval(() => {
+    fetchData()
+  }, 120000)
+})
+
+onUnmounted(() => {
+  if (intervalId) clearInterval(intervalId)
+})
+
 /* const clientBarItems = computed(() => mainStore.clients.slice(0, 4))
 
 const transactionBarItems = computed(() => mainStore.history) */
@@ -48,8 +66,7 @@ const transactionBarItems = computed(() => mainStore.history) */
 <template>
   <LayoutAuthenticated>
     <SectionMain>
-      <SectionTitleLineWithButton :icon="mdiChartTimelineVariant" title="Overview" main>
-      </SectionTitleLineWithButton>
+      <!-- <SectionTitleLineWithButton :icon="mdiChartTimelineVariant" title="Overview" class="-mt-10" />
 
       <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-3">
         <CardBoxWidget
@@ -73,17 +90,33 @@ const transactionBarItems = computed(() => mainStore.history) */
           :number="totalPresenceMonthly"
           label="Rekap Bulanan"
         />
+      </div> -->
+
+      <SectionTitleLineWithButton
+        :icon="mdiAccountMultiple"
+        title="Absensi Hari Ini"
+        class="-mt-12"
+      />
+
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-5 gap-y-4 -mb-10 -mt-2">
+        <CardName
+          v-for="(item, index) in dataPresenceToday"
+          :key="index"
+          :name="item.nama"
+          :check-in="item.waktu_masuk"
+          :last-update="item.terakhir_terlihat"
+        />
       </div>
 
-      <SectionTitleLineWithButton :icon="mdiAccountMultiple" title="Absensi Hari Ini" />
+      <!-- 
 
       <CardBox has-table v-if="totalPresenceToday > 0">
         <TableListPresence :data="dataPresenceToday" />
-      </CardBox>
+      </CardBox> -->
 
-      <CardBox v-else>
+      <!-- <CardBox>
         <CardBoxComponentEmpty text="Belum ada yang absen nih..." />
-      </CardBox>
+      </CardBox> -->
 
       <!-- <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
         <div class="flex flex-col justify-between">
